2a5e1073f4fe57eb910bfd100e5484901ff815eb
Bishal Kumar Jaiswal <077bct022.bishal@pcampus.edu.np>
2025-09-01 17:05:21 +0545

    add purchase ui page made with full functionality

--- IMS/purchase/urls.py
+++ IMS/purchase/urls.py
@@ -2,9 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.purchase,name='purchase'),
-    path('purchase_detail/<int:purchase_id>/',views.purchase_detail,name='purchase_detail'),
-    path('add_purchase/', views.add_purchase, name="add_purchase"),
-    path('supplier_info/<int:supplier_id>/',views.supplier_info,name='supplier_info'),
-
+    path("", views.purchase, name="purchase"),
+    path(
+        "purchase_detail/<int:purchase_id>/",
+        views.purchase_detail,
+        name="purchase_detail",
+    ),
+    path("add_purchase/", views.add_purchase, name="add_purchase"),
+    path("supplier_info/<int:supplier_id>/", views.supplier_info, name="supplier_info"),
+    path("get_products/", views.get_products, name="get_product"),
+    path('get_sizes/<int:product_id>/', views.get_sizes, name='get_sizes'),
+    path('get_lengths/<int:size_id>/', views.get_lengths, name='get_lengths'),
 ]
--- IMS/purchase/views.py
+++ IMS/purchase/views.py
@@ -2,27 +2,64 @@ from django.shortcuts import render
 from .models import Purchase, PurchaseProduct, Supplier
 from .forms import PurchaseForm
 from django.http import JsonResponse
-from stock.models import Product,Size_Variation,Length_Variation
+from stock.models import Product, Size_Variation, Length_Variation
+
+
 # Create your views here.
 def supplier_info(request, supplier_id):
     try:
-      supplier = Supplier.objects.get(id = supplier_id)
-      data = {'vat_no':supplier.vat_no,'phone':supplier.phone, 'address':supplier.address}
-      return JsonResponse(data)
+        supplier = Supplier.objects.get(id=supplier_id)
+        data = {
+            "vat_no": supplier.vat_no,
+            "phone": supplier.phone,
+            "address": supplier.address,
+        }
+        return JsonResponse(data)
     except Supplier.DoesNotExist:
-        return JsonResponse({'error':'Supplier doesn\'t exist'},status=404)
+        return JsonResponse({"error": "Supplier doesn't exist"}, status=404)
+
+
+
+def get_products(request):
+        query = request.GET.get("q", "")  # get the search term from query string
+        if query:
+            products = Product.objects.filter(
+                product_name__icontains=query, is_avialable = True
+            )  # filter by typing
+        else:
+            products = Product.objects.all()  # if nothing typed, return all products
+
+        # prepare data as list of dicts
+        data = [{"id": p.id, "product_name": p.product_name} for p in products]
+        return JsonResponse(data, safe=False)
+
+def get_sizes(request, product_id):
+       sizes = Size_Variation.objects.filter(product_id=product_id)
+       data = [{"id": s.id, "size": s.size} for s in sizes]
+       return JsonResponse(data, safe=False)
+        
+def get_lengths(request, size_id):
+    lengths = Length_Variation.objects.filter(size_id=size_id)
+    data = [{"id": l.id, "length_in_inch": l.length_in_inch} for l in lengths]
+    return JsonResponse(data, safe=False)
 
 def purchase(request):
     purchases = Purchase.objects.all()
-    return render(request,'purchase.html',{'purchases':purchases})
+    return render(request, "purchase.html", {"purchases": purchases})
+
 
 def purchase_detail(request, purchase_id):
-    purchase = Purchase.objects.get(id = purchase_id)
-    return render(request,'purchase_detail.html',{'purchase':purchase})
+    purchase = Purchase.objects.get(id=purchase_id)
+    return render(request, "purchase_detail.html", {"purchase": purchase})
+
 
 def add_purchase(request):
     form = PurchaseForm()
     products = Product.objects.all()
     sizes = Size_Variation.objects.all()
     lengths = Length_Variation.objects.all()
-    return render(request,"add_purchase.html",{"form":form,'products':products,'sizes':sizes,'lengths':lengths})
+    return render(
+        request,
+        "add_purchase.html",
+        {"form": form, "products": products, "sizes": sizes, "lengths": lengths},
+    )
